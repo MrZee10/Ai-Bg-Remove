fbbe5f643667bd4c86b72d866b8d91c68e5b2fe5
MrZee10 <115611264+MrZee10@users.noreply.github.com>
2022-10-12 11:58:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ API_ID = int(os.environ.get("API_ID"))
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', '5253097982').split()]
 FORCE_SUB = os.environ.get("FORCE_SUB")
+RemoveBG_API = os.environ.get('RemoveBG_API', 'HKAErRmcefuqK5DUUoFP9rWu')
