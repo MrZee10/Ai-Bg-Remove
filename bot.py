24070d38f36d58186072acfa87bf7f6f321056d4
MrZee10 <115611264+MrZee10@users.noreply.github.com>
2022-10-12 11:40:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ class TG(Client):
             api_id=API_ID, 
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="ADV_GUARDIAN_GROOT")
+            plugins=dict(root="Ai_Bg_Remover")
         )
 
     async def start(self):
